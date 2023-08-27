a7902d673e72d944f2a42974b68e3184849551a1
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-08-27 13:45:25 -0600

    Time: 1866 ms (32.44%), Space: 16.6 MB (81.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,25 +1,27 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # DP solution
+        # Optimized DP solution
+        # O(sum * n) time
+        # O(sum) space
         def subset_problem(n, target_sum):
+            dp = [False for _ in range(target_sum + 1)]
+            dp[0] = True
             for i in range(1, n + 1):
+                curRow = [False for _ in range(target_sum + 1)]
+                curRow[0] = True
                 for j in range(1, target_sum + 1):
                     if nums[i - 1] <= j:
-                        cache[i][j] = \
-                            (cache[i-1][j - nums[i-1]] or cache[i-1][j])
+                        curRow[j] = dp[j - nums[i-1]] or dp[j]
                     else:
-                        cache[i][j] = cache[i-1][j]
+                        curRow[j] = dp[j]
+                dp = curRow
             
-            return cache[n][target_sum]
+            return dp[target_sum]
         
         n = len(nums)
         sum = 0
         for val in nums:
             sum += val
-        cache = [[False for _ in range(sum + 1)] for _ in range(n + 1)]
-        for i in range(n + 1):
-            cache[i][0] = True
-
         if sum % 2 != 0:
             return False
         else:
