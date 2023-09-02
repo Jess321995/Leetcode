810ca380c1f2a6246f4a500cb7dbb2f331b50d2e
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-09-02 11:40:59 -0600

    Time: 1581 ms (14.63%), Space: 20.7 MB (24.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -9,7 +9,7 @@ class Solution:
             dp[0][j] = float("inf")
         
         for i in range(1, n + 1):
-            for j in range(amount + 1):
+            for j in range(1, amount + 1):
                 if coins[i - 1] <= j:
                     dp[i][j] = \
                         min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j])
