852ae2e585ecf658f2db63f5160e72e047dcb096
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-09-02 11:25:10 -0600

    Time: 1686 ms (11.28%), Space: 59.7 MB (5.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,19 +1,27 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #Memoization
-        @cache
+        # Memoization without cache decorator
+        # O(n * amount) time complexity
+        # O(n * amount) recursive stack space
         def helper(n, amount):
             if amount == 0:
                 return 0
             if n == 0:
                 return float("inf")
+            
+            if cache[n][amount] != -1:
+                return cache[n][amount]
 
             if coins[n - 1] <= amount:
-                return min(1 + helper(n, amount - coins[n - 1]), helper(n - 1, amount))
+                cache[n][amount] = \
+                    min(1 + helper(n, amount - coins[n - 1]),
+                        helper(n - 1, amount))
             else:
-                return helper(n - 1, amount)
-        
-        #cache = [[0 for _ in range(amount + 1)] for _ in range(n + 1)]
+                cache[n][amount] = helper(n - 1, amount)
+            return cache[n][amount]
 
-        val = helper(len(coins), amount)
+        n = len(coins)
+        cache = [[-1 for _ in range(amount + 1)] for _ in range(n + 1)]
+
+        val = helper(n, amount)
         return val if val != float("inf") else -1
