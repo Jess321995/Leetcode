69b86cd6492ced8890e1a8eb3c523bbcb45c0def
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-09-02 11:36:14 -0600

    Time: 1541 ms (16.23%), Space: 20.7 MB (24.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,27 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Memoization without cache decorator
+        # DP (top down)
         # O(n * amount) time complexity
-        # O(n * amount) recursive stack space
-        def helper(n, amount):
-            if amount == 0:
-                return 0
-            if n == 0:
-                return float("inf")
-            
-            if cache[n][amount] != -1:
-                return cache[n][amount]
-
-            if coins[n - 1] <= amount:
-                cache[n][amount] = \
-                    min(1 + helper(n, amount - coins[n - 1]),
-                        helper(n - 1, amount))
-            else:
-                cache[n][amount] = helper(n - 1, amount)
-            return cache[n][amount]
-
+        # O(n * amount) space for tabulation
         n = len(coins)
-        cache = [[-1 for _ in range(amount + 1)] for _ in range(n + 1)]
-
-        val = helper(n, amount)
-        return val if val != float("inf") else -1
+        dp = [[0 for _ in range(amount + 1)] for _ in range(n + 1)]
+        for j in range(1, amount + 1):
+            dp[0][j] = float("inf")
+        
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+                if coins[i - 1] <= j:
+                    dp[i][j] = \
+                        min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j])
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        return dp[n][amount] if dp[n][amount] != float("inf") else -1
