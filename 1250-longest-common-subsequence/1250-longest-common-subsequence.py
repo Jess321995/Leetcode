799cc885b36d52143026e4599a9f1a70d18c3a7d
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-09-03 08:49:42 -0600

    Time: 684 ms (59.81%), Space: 42.5 MB (35.58%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,20 +1,15 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Memoization
+        # DP
         # O(i*j) time complexity
-        # O(i*j) + O(i + j) = cache + recursive stack space complexity
-        def lcs(i, j):
-            if i == 0 or j == 0:
-                return 0
-            if cache[i][j] != -1:
-                return cache[i][j]
-            if text1[i - 1] == text2[j - 1]:
-                cache[i][j] = 1 + lcs(i - 1, j - 1)
-            else:
-                cache[i][j] = max(lcs(i - 1, j), lcs(i, j - 1)) 
-            return cache[i][j]
-
-        cache = [[-1 for _ in range(len(text2) + 1)]
+        # O(i*j) space complexity
+        dp = [[0 for _ in range(len(text2) + 1)]
                  for _ in range(len(text1) + 1)]
-
-        return lcs(len(text1), len(text2))
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):   
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) 
+        
+        return dp[len(text1)][len(text2)]
