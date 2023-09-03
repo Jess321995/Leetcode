47df0339d82fa1eaf3fb675304e7c7924d19586d
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-09-03 08:46:32 -0600

    Time: 942 ms (33.65%), Space: 44.5 MB (23.53%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -3,12 +3,18 @@ class Solution:
         # Memoization
         # O(i*j) time complexity
         # O(i*j) + O(i + j) = cache + recursive stack space complexity
-        @cache
         def lcs(i, j):
             if i == 0 or j == 0:
                 return 0
+            if cache[i][j] != -1:
+                return cache[i][j]
             if text1[i - 1] == text2[j - 1]:
-                return 1 + lcs(i - 1, j - 1)
+                cache[i][j] = 1 + lcs(i - 1, j - 1)
             else:
-                return max(lcs(i - 1, j), lcs(i, j - 1)) 
-        return lcs(len(text1), len(text2))     
+                cache[i][j] = max(lcs(i - 1, j), lcs(i, j - 1)) 
+            return cache[i][j]
+
+        cache = [[-1 for _ in range(len(text2) + 1)]
+                 for _ in range(len(text1) + 1)]
+
+        return lcs(len(text1), len(text2))
