61193ae00c1cbc9493460ffc7c3558f98a9d78e2
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-09-02 22:58:26 -0600

    Time: 130 ms (90.47%), Space: 16.4 MB (94.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -3,19 +3,16 @@ class Solution:
 
         total_sum = sum(nums)
 
-        # Check if it's possible to achieve the target sum based on the total sum
         if (total_sum - target) % 2 != 0 or total_sum < target:
             return 0
 
         subset_sum = (total_sum - target) // 2
 
-        # Create a DP matrix with dimensions (len(nums) + 1) x (subset_sum + 1)
-        dp = [[0] * (subset_sum + 1) for _ in range(len(nums) + 1)]
+        dp = [[0 for _ in range(subset_sum + 1)] for _ in range(len(nums) + 1)]
 
         # There's one way to achieve a sum of 0 (by not selecting any number)
         dp[0][0] = 1
 
-        # Fill in the DP matrix
         for i in range(1, len(nums) + 1):
             for j in range(subset_sum + 1):
                 dp[i][j] = dp[i - 1][j]  # Exclude the current number
