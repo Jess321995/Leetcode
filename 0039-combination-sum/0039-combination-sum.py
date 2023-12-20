df79db73e5e7dff59b925a4c879eab9ba3fcf182
Jess321995 <43223994+Jess321995@users.noreply.github.com>
2023-12-19 23:59:02 -0500

    Time: 66 ms (39.72%), Space: 16.5 MB (16.70%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,29 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # Time complexity: O(2 ^ (t)) where t is the target 
-        # Space complexity: Recursive stack space O(2 ^ (t)) + O(t) to store res
-        output = []
-        def helper(n, target, res):
-            # Found the answer
-            if target == 0:
-                output.append(res.copy())
+        res = []
+        cur = []
+
+        def dfs(i, total):
+            if total == target:
+                res.append(cur.copy())
                 return
-            # Traversed entire array and no items left
-            if n == 0:
+            if i >= len(candidates) or total > target:
                 return
 
-            # Value can be chosen
-            if candidates[n - 1] <= target:
-                # Choose it
-                res.append(candidates[n - 1])
-                helper(n, target - candidates[n - 1], res)
-                # Don't choose it
-                res.pop()
-                helper(n - 1, target, res)
+            cur.append(candidates[i])
+            dfs(i, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, total)
 
-            # Value cannot be chosen
-            else:
-                helper(n - 1, target, res)
-
-        helper(len(candidates), target, [])
-        return output
+        dfs(0, 0)
+        return res
